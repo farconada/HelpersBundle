0f320bc8230c9b5442d2b93d07be670b894afd6b
Fernando Arconada <fernando.arconada@gmail.com>
2015-06-13 16:29:58 +0200

    Body Listener que convierte un objeto son del body en params de symfony

---
+++ EventListener/BodyListener.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fernando
+ * Date: 13/6/15
+ * Time: 12:39
+ */
+
+namespace Fer\HelpersBundle\EventListener;
+
+
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
+
+/**
+ * Si llega un peticion en formato json convierte el cuerpo de la peticion (objeto json) en params de Symfony
+ *
+ * Class BodyListener
+ * @package Fer\HelpersBundle\EventListener
+ */
+class BodyListener {
+
+    /**
+     * Core request handler.
+     *
+     * @param GetResponseEvent $event
+     *
+     * @throws BadRequestHttpException
+     * @throws UnsupportedMediaTypeHttpException
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        $contentType = $request->headers->get('Content-Type');
+        $format = null === $contentType
+            ? $request->getRequestFormat()
+            : $request->getFormat($contentType);
+        $content = $request->getContent();
+
+        if ($format === 'json' && !empty($content)) {
+            $data = @json_decode($content, true);
+            $request->request = new ParameterBag($data);
+        }
+
+    }
+}
